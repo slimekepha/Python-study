9b72467e3b4d729f1d0ca88d809bb26c17737c35
slimekepha <birirgudboy@gmail.com>
2024-11-14 11:00:57 +0300

    Python study

--- random_quiz.py
+++ random_quiz.py
@@ -18,8 +18,41 @@ for i in range(1, 11):
     previous_num = i
 
 
-word=input('enter word:')
-print('original sting is', word)
-size=len(word)
-for i in range(0,size -1, 2):
-    print("Index[",i,"]", word[i] )
+# word=input('enter word:')
+# print('original sting is', word)
+# size=len(word)
+# for i in range(0,size -1, 2):
+#     print("Index[",i,"]", word[i] )
+
+
+word=input("enter the word:")
+print("Original string is",word)
+x=list(word)
+for i in x[0::2]:
+    print(i)
+
+
+x=[10,20,3,3,45,60,30]
+for i in x:
+    if i%5==0:
+        print(i)
+
+
+def first_last_same(numberlist):
+    first_num=numberlist[0]
+
+    second_num=numberlist[-1]
+    if first_num==second_num:
+        return True
+    else:
+        return False
+numx=[20,20,30,20]
+print("result is:",first_last_same(numx))
+
+
+
+
+for i in range(7,0,-1):
+    for j in range(0,i-1):
+        print("*",end='')
+    print("")
